3b4b0ea55f50e8ab51fc4bf5722f8dd97b592121
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-03-19 14:50:07 +0200

    refactored code

--- scripts.js
+++ scripts.js
@@ -42,14 +42,14 @@ statusSet[0].style.color = STATUS_MAP[statusSet[0].innerText].color;
 statusSet[1].style.color = STATUS_MAP[statusSet[1].innerText].color;
 statusSet[2].style.color = STATUS_MAP[statusSet[2].innerText].color;
 
-reserveSet[0].disabled = !STATUS_MAP[statusSet[0].innerText.trim().toLowerCase()].canReserve;
-checkoutSet[0].disabled = !STATUS_MAP[statusSet[0].innerText.trim().toLowerCase()].canCheckout;
-checkinSet[0].disabled = !STATUS_MAP[statusSet[0].innerText.trim().toLowerCase()].canCheckIn;
+reserveSet[0].disabled = !STATUS_MAP[statusSet[0].innerText].canReserve;
+checkoutSet[0].disabled = !STATUS_MAP[statusSet[0].innerText].canCheckout;
+checkinSet[0].disabled = !STATUS_MAP[statusSet[0].innerText].canCheckIn;
 
-reserveSet[1].disabled = !STATUS_MAP[statusSet[1].innerText.trim().toLowerCase()].canReserve;
-checkoutSet[1].disabled = !STATUS_MAP[statusSet[1].innerText.trim().toLowerCase()].canCheckout;
-checkinSet[1].disabled = !STATUS_MAP[statusSet[1].innerText.trim().toLowerCase()].canCheckIn;
+reserveSet[1].disabled = !STATUS_MAP[statusSet[1].innerText].canReserve;
+checkoutSet[1].disabled = !STATUS_MAP[statusSet[1].innerText].canCheckout;
+checkinSet[1].disabled = !STATUS_MAP[statusSet[1].innerText].canCheckIn;
 
-reserveSet[2].disabled = !STATUS_MAP[statusSet[2].innerText.trim().toLowerCase()].canReserve;
-checkoutSet[2].disabled = !STATUS_MAP[statusSet[2].innerText.trim().toLowerCase()].canCheckout;
-checkinSet[2].disabled = !STATUS_MAP[statusSet[2].innerText.trim().toLowerCase()].canCheckIn;
+reserveSet[2].disabled = !STATUS_MAP[statusSet[2].innerText].canReserve;
+checkoutSet[2].disabled = !STATUS_MAP[statusSet[2].innerText].canCheckout;
+checkinSet[2].disabled = !STATUS_MAP[statusSet[2].innerText].canCheckIn;
